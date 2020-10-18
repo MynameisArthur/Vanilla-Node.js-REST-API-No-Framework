caf74cdb60abaf10fe30b12110eaa38abfbfcd2c
Arthur <arthurd800@wp.pl>
2020-10-18 16:10:37 +0200

    created server.js file - main file for the app, created controller and model for the products, added data/products.json, created 2 model methods (findAll and findById), created 2 routes in controller

---
+++ controllers/productController.js
@@ -0,0 +1,43 @@
+const Product = require('../models/productModel');
+
+//@desc Gets All Products
+//@route GET /api/products
+async function getProducts(req, res) {
+    try {
+        const products = await Product.findAll();
+        res.writeHead(200, {
+            'Content-Type': 'application/json',
+        });
+        res.end(JSON.stringify(products));
+    } catch (err) {
+        console.log(err);
+    }
+}
+//@desc Gets Single Product
+//@route GET /api/products/:id
+async function getProduct(req, res, id) {
+    try {
+        const product = await Product.findById(id);
+        if (!product) {
+            res.writeHead(404, {
+                'Content-Type': 'application/json',
+            });
+            res.end(
+                JSON.stringify({
+                    message: 'Product Not Found',
+                })
+            );
+        } else {
+            res.writeHead(200, {
+                'Content-Type': 'application/json',
+            });
+            res.end(JSON.stringify(product));
+        }
+    } catch (err) {
+        console.log(err);
+    }
+}
+module.exports = {
+    getProducts,
+    getProduct,
+};
