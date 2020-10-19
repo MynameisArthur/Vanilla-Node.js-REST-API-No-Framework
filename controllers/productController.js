c92685f6c5e582f7489e310c977c7859d6f8fdfd
Arthur <arthurd800@wp.pl>
2020-10-19 13:29:24 +0200

    created routes, controllers and modle methods for POST and PUT requests

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,4 +1,5 @@
 const Product = require('../models/productModel');
+const {getPostData} = require('../utils');
 
 //@desc Gets All Products
 //@route GET /api/products
@@ -37,7 +38,66 @@ async function getProduct(req, res, id) {
         console.log(err);
     }
 }
+
+//@desc Create a Product
+//@route POST /api/products
+async function createProduct(req, res) {
+    try {
+        const body = await getPostData(req);
+        const {title, description, price} = JSON.parse(body);
+        const product = {
+            title,
+            description,
+            price,
+        };
+
+        const newProduct = await Product.create(product);
+        res.writeHead(201, {
+            'Content-Type': 'application/json',
+        });
+        return res.end(JSON.stringify(newProduct));
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+//@desc Update a Product
+//@route PUT /api/products/:id
+async function updateProduct(req, res, id) {
+    try {
+        const product = await Product.findById(id);
+        if (!product) {
+            res.writeHead(404, {
+                'Content-Type': 'application/json',
+            });
+            res.end(
+                JSON.stringify({
+                    message: 'Product Not Found',
+                })
+            );
+        } else {
+            const body = await getPostData(req);
+            const {title, description, price} = JSON.parse(body);
+            const productData = {
+                title: title || product.title,
+                description: description || product.description,
+                price: price || product.price,
+            };
+
+            const updProduct = await Product.update(id, productData);
+            res.writeHead(200, {
+                'Content-Type': 'application/json',
+            });
+            return res.end(JSON.stringify(updProduct));
+        }
+    } catch (err) {
+        console.log(err);
+    }
+}
+
 module.exports = {
     getProducts,
     getProduct,
+    createProduct,
+    updateProduct,
 };
--- models/productModel.js
+++ models/productModel.js
@@ -1,4 +1,7 @@
 const products = require('../data/products.json');
+const {writeDataToFile} = require('../utils');
+
+const {v4: uuidv4} = require('uuid');
 
 function findAll() {
     return new Promise((resolve, reject) => {
@@ -12,7 +15,29 @@ function findById(id) {
     });
 }
 
+function create(product) {
+    return new Promise((resolve, reject) => {
+        const newProduct = {
+            id: uuidv4(),
+            ...product,
+        };
+        products.push(newProduct);
+        writeDataToFile('./data/products.json', products);
+        resolve(newProduct);
+    });
+}
+function update(id, product) {
+    return new Promise((resolve, reject) => {
+        const index = products.findIndex((p) => p.id === id);
+        products[index] = {id, ...product};
+        writeDataToFile('./data/products.json', products);
+        resolve(products[index]);
+    });
+}
+
 module.exports = {
     findAll,
     findById,
+    create,
+    update,
 };
